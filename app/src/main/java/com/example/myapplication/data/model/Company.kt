623dee333e96cd8956600bd6ac4d2fafa26b431a
Thomas di Gregorio <thomasdigregorio00@outlook.it>
2025-08-08 16:50:24 +0200

    Add Company and User data models
    Introduced Company and User data classes to represent company and user entities in the application. These models will be used for data handling and storage.

---
+++ app/src/main/java/com/example/myapplication/data/model/Company.kt
@@ -0,0 +1,6 @@
+data class Company(
+    val id: String = "",
+    val name: String = "",
+    val email: String = "",
+    val address: String = "",
+    val vatNumber: String = "" 
---
+++ app/src/main/java/com/example/myapplication/data/model/User.kt
@@ -0,0 +1,7 @@
+data class User(
+    val id: String = "",
+    val email: String = "",
+    val password: String = "",
+    val fullName: String = "",
+    val companyId: String = ""
+)
